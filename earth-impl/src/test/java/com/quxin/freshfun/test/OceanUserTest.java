eaf188dc9ec9d0f78365389033b99deebe1f8f38
渠成 <2830224613@qq.com>
2017-01-11 15:46:30 +0800

    给项目配置logback日志

--- earth-impl/src/test/java/com/quxin/freshfun/test/OceanUserTest.java
+++ earth-impl/src/test/java/com/quxin/freshfun/test/OceanUserTest.java
@@ -5,12 +5,15 @@ import com.quxin.freshfun.api.oceanuser.OceanUserService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by ASus on 2017/1/9.
  */
 public class OceanUserTest extends TestBase{
     private OceanUserService oceanUserService;
+    Logger logger = LoggerFactory.getLogger(OceanUserTest.class);
     @Before
     public void setUp() throws Exception {
         oceanUserService = getContext().getBean("oceanUserService", OceanUserService.class);
@@ -23,6 +26,7 @@ public class OceanUserTest extends TestBase{
 
     @Test
     public void test(){
-        System.out.print(JSON.toJSONString(oceanUserService.userLogin("yuexuan","0ba201b1099d8c1c7d7e768998b088d1")));
+        logger.error("测试日志");
+//        System.out.print(JSON.toJSONString(oceanUserService.userLogin("yuexuan","0ba201b1099d8c1c7d7e768998b088d1")));
     }
 }
